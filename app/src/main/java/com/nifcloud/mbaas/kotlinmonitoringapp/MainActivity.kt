8896671e558f75678482d5c08ce1c66893314aeb
tanaka-kokoro <kokoro.tanaka@fujitsu.com>
2023-03-02 20:17:22 +0900

    Enable to receive push notification

--- app/src/main/java/com/nifcloud/mbaas/kotlinmonitoringapp/MainActivity.kt
+++ app/src/main/java/com/nifcloud/mbaas/kotlinmonitoringapp/MainActivity.kt
@@ -2,10 +2,56 @@ package com.nifcloud.mbaas.kotlinmonitoringapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+
+import com.nifcloud.mbaas.core.NCMB
+import com.nifcloud.mbaas.core.NCMBException
+import com.nifcloud.mbaas.core.NCMBObject
+import com.nifcloud.mbaas.core.NCMBCallback
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        NCMB.initialize(this.applicationContext, BuildConfig.NCMB_API_KEY, BuildConfig.NCMB_CLIENT_KEY)
+        // 配信端末の登録
+        NCMB.initializePush(this.applicationContext)
+
+
         setContentView(R.layout.activity_main)
+        askNotificationPermission()
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 }
